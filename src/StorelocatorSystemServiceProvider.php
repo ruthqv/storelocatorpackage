e84326bc357b65efe118a87b6290d15e9541da88
ruthqv <rqvaquero@gmail.com>
2017-12-18 06:05:39 +0100

    tests

--- src/StorelocatorSystemServiceProvider.php
+++ src/StorelocatorSystemServiceProvider.php
@@ -1,21 +1,27 @@
 <?php
 namespace storelocator\storelocatorsystem;
 
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-use storelocator\storelocatorsystem\Models\Store;
 
 class StorelocatorSystemServiceProvider extends ServiceProvider
 {
-  
+
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__.'/views', 'storelocator');
+        $this->loadViewsFrom(__DIR__ . '/views', 'storelocator');
 
         $this->loadRoutesFrom(__DIR__ . '/routes.php');
-        
-    }
 
+        $this->loadMigrationsFrom(__DIR__ . '/migrations');
+
+        $this->publishes([
+            __DIR__ . '/views' => resource_path('views/vendor/storelocator'),
+        ]);
+
+        $this->publishes([
+            __DIR__ . '/migrations' => database_path('migrations'),
+        ], 'migrations');
+    }
 
     public function register()
     {
@@ -23,8 +29,6 @@ class StorelocatorSystemServiceProvider extends ServiceProvider
         $this->app->bind('store', 'storelocator/storelocatorsystem/Models/Store');
         $this->app->make('storelocator\storelocatorsystem\AdminStoresController');
         $this->app->make('storelocator\storelocatorsystem\FrontStoresController');
-
-
 
     }
 }
